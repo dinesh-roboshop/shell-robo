099e65a927a77b5ac6c44d7eaf1afec78da1df58
root <root@ip-172-31-80-244.ec2.internal>
2025-04-16 10:56:54 +0000

    added redis.sh

---
+++ redis.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-TIMESTAMP.log"
+
+if [ $(id -u) -eq 0 ]
+then 
+   echo -e $Y "You are logged in as ROOT user. Proceeding with installation process" $N
+else
+   echo -e $R "You are NOT a ROOT user. Please login as root user to proceed with installation" $N
+   exit 1
+fi
+
+validate ( ) {
+if [ $1 -eq 0 ]
+then
+   echo -e $2 $G "SUCEESS" $N
+else
+   echo -e $2 $R "FAILED" $N
+   exit 1
+fi
+}
+
+#dnf install -y epel-release &>> $LOGFILE
+#validate $? "$(echo -e $Y 'Installing epel-release repository:' $N)"
+
+dnf install -y redis6 &>> $LOGFILE
+validate $? "$(echo -e $Y 'Installing redis package:' $N)"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis6/redis6.conf  &>> $LOGFILE
+validate $? "$(echo -e $Y 'Modifying redis configuration file:' $N)"
+
+systemctl enable redis6  &>> $LOGFILE
+validate $? "$(echo -e $Y 'Enabling redis service:' $N)"
+
+systemctl start redis6  &>> $LOGFILE
+validate $? "$(echo -e $Y 'Starting redis service:' $N)"
