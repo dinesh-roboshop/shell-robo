fca2b6e89272be79ce2f2c71dbed6cbdeee51aeb
root <root@ip-172-31-19-64.ec2.internal>
2025-04-18 12:07:50 +0000

    modified redis file

--- redis.sh
+++ redis.sh
@@ -24,17 +24,20 @@ source ./functions.sh
 
 check_root_user
 
-#dnf install -y epel-release &>> $LOGFILE
-#validate $? "$(echo -e $Y 'Installing epel-release repository:' $N)"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
+validate $? "$(echo -e $Y 'Installing redis repository:' $N)"
 
-dnf install -y redis6 &>> $LOGFILE
+dnf module enable redis:remi-6.2 -y &>> $LOGFILE
+validate $? "$(echo -e $Y 'Enabling redis 6.2 from package stream:' $N)"
+
+dnf install -y redis &>> $LOGFILE
 validate $? "$(echo -e $Y 'Installing redis package:' $N)"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis6/redis6.conf  &>> $LOGFILE
 validate $? "$(echo -e $Y 'Modifying redis configuration file:' $N)"
 
-systemctl enable redis6  &>> $LOGFILE
+systemctl enable redis  &>> $LOGFILE
 validate $? "$(echo -e $Y 'Enabling redis service:' $N)"
 
-systemctl start redis6  &>> $LOGFILE
+systemctl start redis  &>> $LOGFILE
 validate $? "$(echo -e $Y 'Starting redis service:' $N)"
